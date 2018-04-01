34c02813c7eb10876d9a88a58410532ff9c340f1
YuraFedorenko <arhimedyura@gmail.com>
2018-04-01 19:42:43 +0300

    Simple Web Chat without any external libraries

---
+++ WebChat/src/main/java/com/lollion/WebChat/network/TCPConnectionListener.java
@@ -0,0 +1,10 @@
+package com.lollion.WebChat.network;
+
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onException(TCPConnection tcpConnection, Exception e);
+}
